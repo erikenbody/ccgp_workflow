b80fe6820233468266f88d506aa63e653d501288
Cade Mirchandani <cmirchan@ucsc.edu>
2022-04-21 17:59:20 +0000

    cleanup cleanup everybody do ur share

---
+++ workflow/scripts/gcloud_cleanup.py
@@ -0,0 +1,49 @@
+from google.cloud import storage
+import sys
+from os.path import join
+from pprint import pprint
+
+
+def cleanup(bucket_name: str, org: str, ref: str):
+    dirs_to_kill = [
+        join("results", org, ref, x)
+        for x in [
+            "00_fastqFiltered",
+            "01_mappedReads/preMerge",
+            "01_mappedReads/postMerge",
+        ]
+    ]
+    genome = join("data", "genome", ref, "*")
+    fastq = join("data", "fastq", org, "*")
+    dirs_to_kill.extend([genome, fastq])
+
+    cls = storage.Client()
+    bucket = cls.get_bucket(bucket_name)
+
+    blob_list = []
+    for d in dirs_to_kill:
+        blobs = bucket.list_blobs(prefix=d)
+        for blob in blobs:
+            print(f"Would delete file: {blob}")
+            blob_list.append(blob)
+
+    while True:
+        cont = input("Sure you want to delete listed files? y/n: ").lower()
+        if cont == "y":
+            bucket.delete_blobs(blob_list)
+            break
+        elif cont == "n":
+            sys.exit(1)
+        else:
+            cont = input("Sure you want to delete listed files? y/n: ").lower()
+
+
+def main():
+    organism = snakemake.input[0].split("/")[2]
+    ref = snakemake.input[0].split("/")[3]
+    bucket = snakemake.input[0].split("/")[0]
+    cleanup(bucket_name=bucket, org=organism, ref=ref)
+
+
+if __name__ == "__main__":
+    main()
